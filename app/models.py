1641f5ab8170443e7eff0934ece2f59ebb6230f5
Jeff Chebul <jmchebul@jmchebul.com>
2024-04-22 15:24:49 -0500

    first commit, hopefully complete Flask DB

---
+++ app/models.py
@@ -0,0 +1,284 @@
+import secrets
+from . import db
+from datetime import datetime, timezone, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True)
+    first_name = db.Column(db.String(25))
+    last_name = db.Column(db.String(25))
+    street1 = db.Column(db.String(50))
+    street2 = db.Column(db.String(50))
+    city = db.Column(db.String(20))
+    state = db.Column(db.String(20))
+    zip = db.Column(db.Integer)
+    email = db.Column(db.String)
+    phone_number = db.Column(db.Integer)
+    password = db.Column(db.String)
+    private_notes = db.Column(db.String)
+    date_created = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+    pw_hash = db.Column(db.String)
+    is_admin = db.Column(db.Boolean, default=False)
+    token = db.Column(db.String, index=True, unique=True)
+    emergency_contacts = db.relationship('EmergencyContact', backref='user', lazy=True)
+    veterinarians = db.relationship('Veterinarian', backref='user', lazy=True)
+    dogs = db.relationship('Dog', backref='user', lazy=True)
+    images = db.relationship('Images', backref='user', lazy=True)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.set_password(kwargs.get('password', ''))
+
+    def __repr__(self):
+        return f"<User {self.user_id}|{self.first_name} {self.last_name}>"
+    
+    def set_password(self, plaintext_password):
+        self.password = generate_password_hash(plaintext_password)
+        self.save()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def check_password(self, plaintext_password):
+        return check_password_hash(self.password, plaintext_password)
+    
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "street1": self.street1,
+            "street2": self.street2,
+            "city": self.city,
+            "state": self.state,
+            "zip": self.zip,
+            "email": self.email,
+            "phone_number": self.phone_number,
+            "private_notes": self.private_notes,
+            "date_created": self.date_created,
+            "is_admin": self.is_admin,
+            "emergency_contacts": [ec.to_dict() for ec in self.emergency_contacts],
+            "veterinarians": [vet.to_dict() for vet in self.veterinarians],
+            "dogs": [dog.to_dict() for dog in self.dogs],
+            "images": [image.to_dict() for image in self.images]
+        }
+    
+    def get_token(self):
+        self.token = secrets.token_hex(16)
+        self.save()
+        return {"token": self.token}
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, **kwargs):
+        allowed_fields = {'first_name', 'last_name', 'street1', 'street2', 'city', 'state', 'zip', 'email', 'phone_number', 'private_notes', 'is_admin'}
+
+        for key, value in kwargs.items():
+            if key in allowed_fields:
+                setattr(self, key, value)
+        self.save()
+
+
+
+class EmergencyContact(db.Model):
+    ec_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    phone_number = db.Column(db.Integer)
+    email = db.Column(db.String(50))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<EmergencyContact {self.ec_id}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, **kwargs):
+        allowed_fields = {'first_name', 'last_name', 'phone_number', 'email'}
+
+        for key, value in kwargs.items():
+            if key in allowed_fields:
+                setattr(self, key, value)
+        self.save()
+
+    def to_dict(self):
+        return {
+            "ec_id": self.ec_id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "phone_number": self.phone_number,
+            "email": self.email
+        }
+
+
+class Veterinarian(db.Model):
+    vet_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25))
+    clinic = db.Column(db.String(25))
+    street1 = db.Column(db.String(50))
+    street2 = db.Column(db.String(50))
+    city = db.Column(db.String(20))
+    state = db.Column(db.String(20))
+    zip = db.Column(db.Integer)
+    email = db.Column(db.String)
+    phone_number = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<Veterinarian {self.vet_id}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, **kwargs):
+        allowed_fields = {'name', 'clinic', 'street1', 'street2', 'city', 'state', 'zip', 'email', 'phone_number'}
+
+        for key, value in kwargs.items():
+            if key in allowed_fields:
+                setattr(self, key, value)
+        self.save()
+
+    def to_dict(self):
+        return {
+            "vet_id": self.vet_id,
+            "name": self.name,
+            "clinic": self.clinic,
+            "street1": self.street1,
+            "street2": self.street2,
+            "city": self.city,
+            "state": self.state,
+            "zip": self.zip,
+            "email": self.email,
+            "phone_number": self.phone_number
+        }
+
+
+class Dog(db.Model):
+    dog_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    breed = db.Column(db.String)
+    sex = db.Column(db.String)
+    altered = db.Column(db.Boolean)
+    health_conditions = db.Column(db.String)
+    medications = db.Column(db.String)
+    allergies = db.Column(db.String)
+    private_notes = db.Column(db.String)
+    bn_favorite_activities = db.Column(db.String)
+    bn_issues = db.Column(db.String)
+    profile_pic_url = db.Column(db.String)
+    feeding_schedule = db.Column(db.String)
+    potty_schedule = db.Column(db.String)
+    crated = db.Column(db.Boolean)
+    daily_updates = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    vet_id = db.Column(db.Integer, db.ForeignKey('veterinarian.vet_id'))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<Dog {self.dog_id}|{self.name}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, **kwargs):
+        allowed_fields = {'name', 'breed', 'sex', 'altered', 'health_conditions', 'medications', 'allergies', 'private_notes', 'bn_favorite_activities', 'bn_issues', 'profile_pic_url', 'feeding_schedule', 'potty_schedule', 'crated', 'daily_updates'}
+
+        for key, value in kwargs.items():
+            if key in allowed_fields:
+                setattr(self, key, value)   
+        self.save()
+
+    def to_dict(self):
+        return {
+            "dog_id": self.dog_id,
+            "name": self.name,
+            "breed": self.breed,
+            "sex": self.sex,
+            "altered": self.altered,
+            "health_conditions": self.health_conditions,
+            "medications": self.medications,
+            "allergies": self.allergies,
+            "private_notes": self.private_notes,
+            "bn_favorite_activities": self.bn_favorite_activities,
+            "bn_issues": self.bn_issues,
+            "profile_pic_url": self.profile_pic_url,
+            "feeding_schedule": self.feeding_schedule,
+            "potty_schedule": self.potty_schedule,
+            "crated": self.crated,
+            "daily_updates": self.daily_updates
+        }
+        
+
+
+class Images(db.Model):
+    image_id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String)
+    date_added = db.Column(db.DateTime(timezone=True), default=datetime.now)
+    uploaded_by_user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.save()
+
+    def __repr__(self):
+        return f"<Images {self.image_id}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, **kwargs):
+        allowed_fields = {'image_url', 'date_added'}
+
+        for key, value in kwargs.items():
+            if key in allowed_fields:
+                setattr(self, key, value)
+        self.save()
+
+    def to_dict(self):
+        return {
+            "image_id": self.image_id,
+            "image_url": self.image_url,
+            "uploaded_by_user_id": self.uploaded_by_user_id,
+            "date_added": self.date_added
+        }
+  
---
+++ app/routes.py
@@ -0,0 +1,260 @@
+from flask import request, render_template
+from app import app, db
+from .models import User, EmergencyContact, Veterinarian, Dog, Image
+from .auth import admin_required  # Import the admin_required decorator
+
+
+# Define a route
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+#User endpoints
+
+#create a  new user
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    #Check to make sure that the request body is JSON
+    if not request.is_json:
+        return {'error': 'Your content-type must be application/json'}, 400
+    # GET the data from the request body
+    data = request.json
+
+    #Validate that the data has all the required fields
+    required_fields = ['firstName', 'lastName', 'username', 'email', 'password']
+    missing_fields = []
+    for field in required_fields:
+        if field not in data:
+            missing_fields.append(field)
+    if missing_fields:
+        return {'error': f"{', '.join(missing_fields)} must be in the request body"}, 400
+    
+    # Pull the individual data from the body
+    first_name = data.get('firstName')
+    last_name = data.get('lastName')
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    # Check to see if any current users already have that username and/or email
+    check_users = db.session.execute(db.select(User).where( (User.username == username) | (User.email == email) )).scalars().all()
+    if check_users:
+        return {'error': "A user with that username and/or email already exists"}, 400 
+        
+    # Create a new instance of user with the data from the request
+    new_user = User(first_name=first_name, last_name=last_name, email=email, password=password, username=username)
+    
+    return new_user.to_dict(), 201
+
+
+
+@app.route('/users/me', method=['PUT'])
+@token_auth.login_required
+def update_me():
+    user = token_auth.current_user()
+    data = request.json
+    user.update(**data)
+    return user.to_dict() 
+
+@app.route('/users/me', method=['DELETE'])
+@token_auth.login_required
+def delete_me():
+    user = token_auth.current_user()
+    user.delete()
+    return {'success': 'User has been successfully deleted'}, 200
+
+@app.route('/users/me', method=['GET'])
+@token_auth.login_required
+def get_me():
+    user = token_auth.current_user()
+    return user.to_dict()
+
+# Log In endpoint
+
+@app.route('/login', methods=['GET'])
+@basic_auth.verify_password
+def login():
+    user = basic_auth.user
+    return user.get_token()
+
+
+
+# Image endpoints
+
+@app.route('/images', methods=['POST'])
+@token_auth.login_required
+@admin_required
+def create_image():
+    data = request.json
+    user = token_auth.current_user()
+    image = Image(uploaded_by_user_id=user.user_id, **data)
+    return image.to_dict(), 201
+
+@app.route('/images/<int:image_id>', methods=['GET'])
+@token_auth.login_required
+def get_image(image_id):
+    image = db.session.execute(db.select(Image).where(Image.image_id == image_id)).scalar_one_or_none()
+    if image is None:
+        return {'error': 'Image not found'}, 404
+    return image.to_dict()
+
+@app.route('/images/<int:image_id>', methods=['DELETE'])
+@token_auth.login_required
+@admin_required
+def delete_image(image_id):
+    image = db.session.execute(db.select(Image).where(Image.image_id == image_id)).scalar_one_or_none()
+    if image is None:
+        return {'error': 'Image not found'}, 404
+    image.delete()
+    return {'success': 'Image has been successfully deleted'}, 200
+
+@app.route('/images', methods=['GET'])
+@token_auth.login_required
+def get_images():
+    images = db.session.execute(db.select(Image)).scalars().all()
+    return [image.to_dict() for image in images]
+
+# Emergency Contact endpoints
+
+@app.route('/emergency-contacts', methods=['POST'])
+@token_auth.login_required
+def create_emergency_contact():
+    data = request.json
+    user = token_auth.current_user()
+    emergency_contact = EmergencyContact(user_id=user.user_id, **data)
+    return emergency_contact.to_dict(), 201
+
+@app.route('/emergency-contacts/<int:emergency_contact_id>', methods=['GET'])
+@token_auth.login_required
+def get_emergency_contact(emergency_contact_id):
+    emergency_contact = db.session.execute(db.select(EmergencyContact).where(EmergencyContact.ec_id == emergency_contact_id)).scalar_one_or_none()
+    if emergency_contact is None:
+        return {'error': 'Emergency contact not found'}, 404
+    return emergency_contact.to_dict()
+
+@app.route('/emergency-contacts/<int:emergency_contact_id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_emergency_contact(emergency_contact_id):
+    emergency_contact = db.session.execute(db.select(EmergencyContact).where(EmergencyContact.ec_id == emergency_contact_id)).scalar_one_or_none()
+    if emergency_contact is None:
+        return {'error': 'Emergency contact not found'}, 404
+    emergency_contact.delete()
+    return {'success': 'Emergency contact has been successfully deleted'}, 200
+
+@app.route('/emergency-contacts', methods=['GET'])
+@token_auth.login_required
+def get_emergency_contacts():
+    emergency_contacts = db.session.execute(db.select(EmergencyContact)).scalars().all()
+    return [emergency_contact.to_dict() for emergency_contact in emergency_contacts]
+
+@app.route('/emergency-contacts/<int:emergency_contact_id>', methods=['PUT'])
+@token_auth.login_required
+def update_emergency_contact(emergency_contact_id):
+    emergency_contact = db.session.execute(db.select(EmergencyContact).where(EmergencyContact.ec_id == emergency_contact_id)).scalar_one_or_none()
+    if emergency_contact is None:
+        return {'error': 'Emergency contact not found'}, 404
+    
+    data = request.json
+    emergency_contact.update(**data)
+    return emergency_contact.to_dict()
+
+# Veterinarian endpoints
+
+@app.route('/veterinarians', methods=['POST'])
+@token_auth.login_required
+def create_veterinarian():
+    data = request.json
+    user = token_auth.current_user()
+    veterinarian = Veterinarian(user_id=user.user_id, **data)
+    return veterinarian.to_dict(), 201
+
+@app.route('/veterinarians/<int:veterinarian_id>', methods=['GET'])
+@token_auth.login_required
+def get_veterinarian(veterinarian_id):
+    veterinarian = db.session.execute(db.select(Veterinarian).where(Veterinarian.vet_id == veterinarian_id)).scalar_one_or_none()
+    if veterinarian is None:
+        return {'error': 'Veterinarian not found'}, 404
+    return veterinarian.to_dict()
+
+@app.route('/veterinarians/<int:veterinarian_id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_veterinarian(veterinarian_id):
+    veterinarian = db.session.execute(db.select(Veterinarian).where(Veterinarian.vet_id == veterinarian_id)).scalar_one_or_none()
+    if veterinarian is None:
+        return {'error': 'Veterinarian not found'}, 404
+    veterinarian.delete()
+    return {'success': 'Veterinarian has been successfully deleted'}, 200
+
+@app.route('/veterinarians', methods=['GET'])
+@token_auth.login_required
+def get_veterinarians():
+    veterinarians = db.session.execute(db.select(Veterinarian)).scalars().all()
+    return [veterinarian.to_dict() for veterinarian in veterinarians]
+
+@app.route('/veterinarians/<int:veterinarian_id>', methods=['PUT'])
+@token_auth.login_required
+def update_veterinarian(veterinarian_id):
+    veterinarian = db.session.execute(db.select(Veterinarian).where(Veterinarian.vet_id == veterinarian_id)).scalar_one_or_none()
+    if veterinarian is None:
+        return {'error': 'Veterinarian not found'}, 404
+    
+    data = request.json
+    veterinarian.update(**data)
+    return veterinarian.to_dict()
+
+# Dog endpoints
+
+@app.route('/dogs', methods=['POST'])
+@token_auth.login_required
+def create_dog():
+    data = request.json
+    user = token_auth.current_user()
+    dog = Dog(user_id=user.user_id, **data)
+    return dog.to_dict(), 201
+
+@app.route('/dogs/<int:dog_id>', methods=['GET'])
+@token_auth.login_required
+def get_dog(dog_id):
+    dog = db.session.execute(db.select(Dog).where(Dog.dog_id == dog_id)).scalar_one_or_none()
+    if dog is None:
+        return {'error': 'Dog not found'}, 404
+    return dog.to_dict()
+
+@app.route('/dogs/<int:dog_id>', methods=['DELETE'])
+@token_auth.login_required
+def delete_dog(dog_id):
+    dog = db.session.execute(db.select(Dog).where(Dog.dog_id == dog_id)).scalar_one_or_none()
+    if dog is None:
+        return {'error': 'Dog not found'}, 404
+    dog.delete()
+    return {'success': 'Dog has been successfully deleted'}, 200
+
+@app.route('/dogs', methods=['GET'])
+@token_auth.login_required
+def get_dogs():
+    dogs = db.session.execute(db.select(Dog)).scalars().all()
+    return [dog.to_dict() for dog in dogs]
+
+@app.route('/dogs/<int:dog_id>', methods=['PUT'])
+@token_auth.login_required
+def update_dog(dog_id):
+    dog = db.session.execute(db.select(Dog).where(Dog.dog_id == dog_id)).scalar_one_or_none()
+    if dog is None:
+        return {'error': 'Dog not found'}, 404
+    
+    data = request.json
+    dog.update(**data)
+    return dog.to_dict()
+
+
+
+
+
+
+
+
+
+
+
+
